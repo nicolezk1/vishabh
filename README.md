88c8e9f5248c7d58496e5044d4fcaffd591467af
Nicole Zafalon Kovacs <nicole@ibm.com>
2019-11-04 11:39:02 -0300

    added LIME analysis

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
-# vishabh
-Breast Cancer data analysis
+# Prediction on Breast Cancer Dataset and Explainability using LIME
+This project is intended to demonstrate data analysis concepts based on breast cancer data. The project was forked in order to add explainability to the classigication model using the LIME library. For more information on the original analysis, check:
+https://towardsdatascience.com/building-a-simple-machine-learning-model-on-breast-cancer-data-eca4b3b99fa3
+
+Dataset
+---
+Breast Cancer Wisconsin (Diagnostic) Data Set available at either of the links below:
 http://archive.ics.uci.edu/ml/datasets/breast+cancer+wisconsin+%28diagnostic%29
+https://www.kaggle.com/uciml/breast-cancer-wisconsin-data
+
+Dataset Attribute information
+---
+
+1) ID number
+2) Diagnosis (M = malignant, B = benign)
+3-32)
+
+Ten real-valued features are computed for each cell nucleus:
+
+	a) radius (mean of distances from center to points on the perimeter)
+	b) texture (standard deviation of gray-scale values)
+	c) perimeter
+	d) area
+	e) smoothness (local variation in radius lengths)
+	f) compactness (perimeter^2 / area - 1.0)
+	g) concavity (severity of concave portions of the contour)
+	h) concave points (number of concave portions of the contour)
+	i) symmetry 
+	j) fractal dimension ("coastline approximation" - 1)
+
+Several of the papers listed above contain detailed descriptions of
+how these features are computed. 
+
+The mean, standard error, and "worst" or largest (mean of the three
+largest values) of these features were computed for each image,
+resulting in 30 features.  For instance, field 3 is Mean Radius, field
+13 is Radius SE, field 23 is Worst Radius.
+
+All feature values are recoded with four significant digits.
--- breast_cancer_analysis.py
+++ breast_cancer_analysis.py
@@ -1,38 +1,106 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 29 01:04:23 2018
+#!/usr/bin/env python
+# coding: utf-8
 
-@author: 1022316
-"""
+# In[ ]:
 
-#Classification of cancer dignosis
-#importing the libraries
+
+# Original jupyter notebook from https://github.com/vishabh123/vishabh
+
+
+# In[ ]:
+
+
+# Importing Libraries
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 
-#importing the dataset 
-dataset = pd.read_csv('C:\Machine learning\cancer_data\cancer.csv')
-X = dataset.iloc[:, 1:31].values
-Y = dataset.iloc[:, 31].values
+
+# In[ ]:
+
+
+# Reading data
+
+# Data obtained from https://www.kaggle.com/uciml/breast-cancer-wisconsin-data
+dataset = pd.read_csv('data 2.csv')
+
+
+# **Features** 
+# 
+# 1) ID number
+# 
+# 2) Diagnosis (M = malignant, B = benign)
+# 
+# 3-32)
+# 
+# Ten real-valued features are computed for each cell nucleus:
+# 
+# 	a) radius (mean of distances from center to points on the perimeter)
+# 	b) texture (standard deviation of gray-scale values)
+# 	c) perimeter
+# 	d) area
+# 	e) smoothness (local variation in radius lengths)
+# 	f) compactness (perimeter^2 / area - 1.0)
+# 	g) concavity (severity of concave portions of the contour)
+# 	h) concave points (number of concave portions of the contour)
+# 	i) symmetry 
+# 	j) fractal dimension ("coastline approximation" - 1)
+# 
+# 
+# The mean, standard error, and "worst" or largest (mean of the three
+# largest values) of these features were computed for each image,
+# resulting in 30 features.
+
+# In[ ]:
+
 
 dataset.head()
 
+
+# In[ ]:
+
+
 print("Cancer data set dimensions : {}".format(dataset.shape))
+
+
+# In[ ]:
+
 
 dataset.groupby('diagnosis').size()
 
-#Visualization of data
-dataset.groupby('diagnosis').hist(figsize=(12, 12))
 
-dataset.isnull().sum()
+# In[ ]:
+
+
+# Visualization of data
+
+fig, axes = plt.subplots(ncols=10, nrows=3, figsize=(30,10))
+
+for c, ax in zip(dataset.columns[2::], axes.flat):
+    sns.distplot(dataset[dataset.diagnosis=='B'][c], ax=ax, color='g', label='benign')
+    sns.distplot(dataset[dataset.diagnosis=='M'][c], ax=ax, color='r', label='malignant')
+plt.legend()
+plt.show()
+
+
+# In[ ]:
+
+
 dataset.isna().sum()
 
-dataframe = pd.DataFrame(Y)
-#Encoding categorical data values 
-from sklearn.preprocessing import LabelEncoder
-labelencoder_Y = LabelEncoder()
-Y = labelencoder_Y.fit_transform(Y)
+
+# In[ ]:
+
+
+dataset.drop(columns=['Unnamed: 32'], inplace=True)
+dataset.diagnosis.replace({'B':0,'M':1}, inplace=True)
+
+X = dataset.iloc[:, 2:32].values
+Y = dataset.iloc[:, 1].values
+
+
+# In[ ]:
 
 
 # Splitting the dataset into the Training set and Test set
@@ -40,47 +108,58 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
 
 
-#Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+# In[ ]:
 
-#Fitting the Logistic Regression Algorithm to the Training Set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
-classifier.fit(X_train, Y_train)
-#95.8 Acuracy
 
-#Fitting K-NN Algorithm
-from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
-classifier.fit(X_train, Y_train)
-#95.1 Acuracy
+# Feature Scaling
+# from sklearn.preprocessing import StandardScaler
+# sc = StandardScaler()
+# X_train = sc.fit_transform(X_train)
+# X_test = sc.transform(X_test)
 
-#Fitting SVM
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'linear', random_state = 0)
-classifier.fit(X_train, Y_train) 
-#97.2 Acuracy
 
-#Fitting K-SVM
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'rbf', random_state = 0)
-classifier.fit(X_train, Y_train)
-#96.5 Acuracy
+# In[ ]:
 
-#Fitting Naive_Bayes
-from sklearn.naive_bayes import GaussianNB
-classifier = GaussianNB()
-classifier.fit(X_train, Y_train)
-#91.6 Acuracy
 
-#Fitting Decision Tree Algorithm
-from sklearn.tree import DecisionTreeClassifier
-classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
-classifier.fit(X_train, Y_train)
-#95.8 Acuracy
+# #Fitting the Logistic Regression Algorithm to the Training Set
+# from sklearn.linear_model import LogisticRegression
+# classifier = LogisticRegression(random_state = 0)
+# classifier.fit(X_train, Y_train)
+# #95.8 Acuracy
+
+# #Fitting K-NN Algorithm
+# from sklearn.neighbors import KNeighborsClassifier
+# classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+# classifier.fit(X_train, Y_train)
+# #95.1 Acuracy
+
+# #Fitting SVM
+# from sklearn.svm import SVC
+# classifier = SVC(kernel = 'linear', random_state = 0)
+# classifier.fit(X_train, Y_train) 
+# #97.2 Acuracy
+
+# #Fitting K-SVM
+# from sklearn.svm import SVC
+# classifier = SVC(kernel = 'rbf', random_state = 0)
+# classifier.fit(X_train, Y_train)
+# #96.5 Acuracy
+
+# #Fitting Naive_Bayes
+# from sklearn.naive_bayes import GaussianNB
+# classifier = GaussianNB()
+# classifier.fit(X_train, Y_train)
+# #91.6 Acuracy
+
+# #Fitting Decision Tree Algorithm
+# from sklearn.tree import DecisionTreeClassifier
+# classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+# classifier.fit(X_train, Y_train)
+# #95.8 Acuracy
+
+
+# In[ ]:
+
 
 #Fitting Random Forest Classification Algorithm
 from sklearn.ensemble import RandomForestClassifier
@@ -88,17 +167,46 @@ classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', ra
 classifier.fit(X_train, Y_train)
 #98.6 Acuracy
 
+
+# In[ ]:
+
+
 #predicting the Test set results
 Y_pred = classifier.predict(X_test)
 
-#Creating the confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(Y_test, Y_pred)
-c = print(cm[0, 0] + cm[1, 1])
+from sklearn.metrics import accuracy_score
+# Train and Test Accuracy
+print("Train Accuracy :: " + str(accuracy_score(Y_train, classifier.predict(X_train))))
+print("Test Accuracy  :: " + str(accuracy_score(Y_test, Y_pred)))
 
 
+# In[ ]:
 
 
+import lime
+import lime.lime_tabular
+
+explainer = lime.lime_tabular.LimeTabularExplainer(X_train, class_names=['benign','malignant'], feature_names=dataset.iloc[:, 2:32].columns)
+predict_fn = lambda x: classifier.predict_proba(x)
+
+
+# In[ ]:
+
+
+for i in [30,31,32]:
+    exp = explainer.explain_instance(X_test[i], predict_fn, num_features=5)
+    exp.show_in_notebook()
+
+
+# In[ ]:
+
+
+df = pd.DataFrame(X_test,columns=dataset.iloc[:, 2:32].columns)
+df['diagnosis'] = Y_test
+df.loc[30:32,:]
+
+
+# In[ ]:
 
 
 
